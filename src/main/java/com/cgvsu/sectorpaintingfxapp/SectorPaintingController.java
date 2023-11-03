e33737cf0b20312fefc17361bbb7b3e0ade55cd4
nagornyy_a_s <nagornyi.arsenii@mail.ru>
2023-11-04 01:20:33 +0300

    Implement drawSector, isPointInSector methods

--- src/main/java/com/cgvsu/sectorpaintingfxapp/SectorPaintingController.java
+++ src/main/java/com/cgvsu/sectorpaintingfxapp/SectorPaintingController.java
@@ -2,6 +2,10 @@ package com.cgvsu.sectorpaintingfxapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 import javafx.scene.layout.AnchorPane;
 
 public class SectorPaintingController {
@@ -10,21 +14,53 @@ public class SectorPaintingController {
     @FXML
     private Canvas canvas;
 
-    double centerX;
-    double centerY;
-    double radius;
-    double startAngle;
-    double length;
+    double centerX = 150;
+    double centerY = 150;
+    double radius = 100;
+    double startAngle = 340;
+    double length = 45;
+    double endAngle = startAngle + length;
 
     public void initialize() {
         drawSector();
     }
 
     private void drawSector() {
+        WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
 
+        for (int y = (int) (centerY-radius); y < centerY+radius; y++) {
+            for (int x = (int) (centerX-radius); x < centerX+radius; x++) {
+                if (isPointInSector(x, y)) {
+                    pixelWriter.setColor(x, y, Color.RED);
+                }
+            }
+        }
+
+        canvas.getGraphicsContext2D().drawImage(writableImage, 0, 0);
     }
 
-//    private boolean isPointInSector(int x, int y) {
-//
-//    }
+    private boolean isPointInSector(int x, int y) {
+        double angle = Math.toDegrees(Math.atan2(y - centerY, x - centerX));
+        if (angle < 0) {
+            angle += 360;
+        }
+
+        double distance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2));
+
+        // Корректировка углов для обеспечения правильного диапазона от 0 до 360 градусов
+        if (startAngle > 360) {
+            startAngle %= 360;
+            endAngle = (startAngle + length) % 360;
+        }
+
+        // Проверка на переход через 360 градусов
+        if (endAngle > 360) {
+            if (angle >= startAngle || angle <= endAngle - 360) {
+                return distance <= radius;
+            }
+        }
+
+        return angle >= startAngle && angle <= endAngle && distance <= radius;
+    }
 }
